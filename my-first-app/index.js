33577f1d4be0680af64e361cc6f774c75fb176fb
Denys Vuika <denys.vuika@gmail.com>
2019-02-27 17:53:45 +0000

    chapter 1: my first app

---
+++ my-first-app/index.js
@@ -0,0 +1,12 @@
+// 1.
+const { app, BrowserWindow } = require('electron');
+
+// 2.
+let window;
+
+// 3.
+app.on('ready', () => {
+    // 4.
+    window = new BrowserWindow({ width: 800, height: 600 });
+    window.loadFile('index.html');
+});
