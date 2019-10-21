666ae81a4dafc8229a133140b83bd9c5c7bdb78c
Denys Vuika <denys.vuika@gmail.com>
2019-10-21 20:24:37 +0100

    upgrade my-first-app to latest libs

--- my-first-app/index.js
+++ my-first-app/index.js
@@ -6,7 +6,13 @@ let window;
 
 // 3.
 app.on('ready', () => {
-    // 4.
-    window = new BrowserWindow({ width: 800, height: 600 });
-    window.loadFile('index.html');
+  // 4.
+  window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  window.loadFile('index.html');
 });
