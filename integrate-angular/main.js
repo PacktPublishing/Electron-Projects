e1b5f506f81f96570296037261d6cd8187f5f965
Denys Vuika <denys.vuika@gmail.com>
2019-10-27 10:46:14 +0000

    upgrade 'integrate-angular'

--- integrate-angular/main.js
+++ integrate-angular/main.js
@@ -6,7 +6,13 @@ let win;
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 });
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
 
   // and load the index.html of the app.
   if (process.env.DEBUG) {
