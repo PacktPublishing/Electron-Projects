6cf1ada0968e4a8ec14b9c4c6ef338a63db36363
Denys Vuika <denys.vuika@gmail.com>
2019-11-10 17:35:57 +0000

    update crypto-wallet example

--- crypto-wallet/public/electron.js
+++ crypto-wallet/public/electron.js
@@ -1,4 +1,6 @@
 const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -10,7 +12,11 @@ function createWindow() {
     resizable: false
   });
 
-  win.loadURL(`http://localhost:3000`);
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : 'index.html'
+  );
 }
 
 app.on('ready', createWindow);
